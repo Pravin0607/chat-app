06921b0a39cf7d9c5c4fbbdcc8040b7d05d97320
Pravin Adhav <pravinadhav4945@gmail.com>
2025-08-19 14:29:32 +0530

    Initialize server with TypeScript setup, including package.json, tsconfig.json, and a basic index.ts file.

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center flex-col gap-4">
+      <h1 className="">Initial App</h1>
+      <Button className="cursor-pointer">Get Started</Button>
+    </div>
+  )
+}
+
+export default App
